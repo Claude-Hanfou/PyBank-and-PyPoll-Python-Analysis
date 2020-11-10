a1ed788e961bd4b524055500d668176564363a89
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2020-11-10 00:20:49 -0600

    csvfiles

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,14 +4,14 @@ import csv
 # Set path for file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#List to iterate through each row and store data 
+#Create list to iterate through each row and store data 
 
 total_months = []
 total_profit = []
-monthly_pc = []
+monthly_change = []
 
 # Open the CSV
-with open(csvpath) as csvfile: 
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
      #Reading the header row
@@ -20,34 +20,51 @@ with open(csvpath) as csvfile:
     #Loop through every row
     for row in csvreader:
 
-    #Appens the number of months to it's list 
+    #Appens the number of months to it's list cal length when printing 
      total_months.append(row[0])
 
-     #Append the total to it's list
+     #Append the total to it's list, cal sum when printing
      total_profit.append(int(row[1]))
 
-     #Calculate the differnce in PL and find average 
-     for i in range(len(total_profit)-1):
-         monthly_pc.append(total_profit[i+1]-total_profit[i])
+        # Go through the row to have the monthly change
+    for i in range(len(total_profit)-1):
+        
+        # To Calculate monthly change
+        monthly_change.append(total_profit[i+1]-total_profit[i])
+        
+# Obtain the max and min for the  monthly_profit_change
+max_increase_value = max(monthly_change)
+max_decrease_value = min(monthly_change)
 
-#The maximum and minimum in the monthly change
+# Use list and index from max and min monthly_fit_change
+max_increase_month = monthly_change.index(max(monthly_change)) + 1
+max_decrease_month = monthly_change.index(min(monthly_change)) + 1 
 
-#We obtain values for the max and min value
-max_increase_value = max(monthly_pc)
-max_decrease_value = min(monthly_pc)
-
-# We use the month list and index and add +1 for the for the next month
-
-max_increase_month = monthly_pc.index(max(monthly_pc)) + 1
-max_decrease_month = monthly_pc.index(min(monthly_pc)) + 1 
-
-
-#Printing above statements
+# Printing the above statements 
 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {len(total_months)}")
 print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthly_pc)/len(monthly_pc),2)}")
+print(f"Average Change: {round(sum(monthly_change)/len(monthly_change),2)}")
 print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
 print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+#output files as test files
+csvpath = os.path.join('Analysis', 'budget_data.csv')
+
+#Printing results as text file 
+
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
